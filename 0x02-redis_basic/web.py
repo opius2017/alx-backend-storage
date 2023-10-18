9595af95e462cb1f4dad0ecc4488cd832464b766
Owolabi Pius <opius2007@gmail.com>
2023-10-18 17:20:31 +0100

    redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,47 +1,22 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+""" Implementing an expiring web cache and tracker
+    obtain the HTML content of a particular URL and returns it """
 import redis
 import requests
-from functools import wraps
-from typing import Callable
+r = redis.Redis()
+count = 0
 
-redis_store = redis.StrictRedis()
-'''The module-level Redis instance.
-'''
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        # Increment the access count
-        access_count_key = f'count:{url}'
-        redis_store.incr(access_count_key)
-        
-        # Check if the data is cached
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        
-        # If not cached, fetch and cache the data
-        result = method(url)
-        redis_store.setex(f'result:{url}', 10, result)
-        
-        return result
-
-    return invoker
-
-@data_cacher
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """ track how many times a particular URL was accessed in the key
+        "count:{url}"
+        and cache the result with an expiration time of 10 seconds """
+    r.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    return resp.text
+
 
 if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/10000/url/http://www.google.com"
-    page = get_page(url)
-    print(page)
+    get_page('http://slowwly.robertomurray.co.uk')
